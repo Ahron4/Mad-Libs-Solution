96bc595819cbce08e4f6c184283e0418812ae263
Ahron4 <ahron.natividad12@gmail.com>
2024-12-26 22:16:15 -1000

    commit to "main"

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function story1()
+function dinner()
 {
-    
+    const food = prompt("Food");
+    const smthStrange = prompt ("Something strange");
+    const adj = prompt("Adjective");
+    const animal = prompt("Animal");
+    const verb = prompt("Verb ending in -ing");
+    const obj = prompt("Object");
+    const funQuote1 = prompt("A funny quote");
+    const funQuote2 = prompt("Another funny quote");
+
+    if (food && smthStrange && adj && animal && verb && obj && funQuote1 && funQuote2)
+    {
+        /* Already implemented, missing text and input */
+        document.getElementById("output").innerHTML = `
+        <p>The dinner party started with a dish of ${food} that smelled like ${smthStrange}. 
+        Suddenly, a ${adj} ${animal} appeared and started ${verb} on the table! 
+        The host grabbed a ${obj} and yelled "${funQuote1}!" 
+        We all laughed and said "${funQuote2}." It was unforgettable!</p>
+        `;
+    }
+    /* Error message goes here */
+    else
+    {
+        alert("Oops! You missed one or more inputs. Please try again!");
+    }
 }
